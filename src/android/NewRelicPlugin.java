e2aaca4687d1f59d0458be8d82d225d72f5ec464
Jared Armstrong <jarmstrong@omnispear.com>
2014-06-26 15:33:07 -0400

    add missing imports

--- src/android/NewRelicPlugin.java
+++ src/android/NewRelicPlugin.java
@@ -2,6 +2,8 @@ package com.newrelic.cordova;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import com.newrelic.agent.android.NewRelic;
 
